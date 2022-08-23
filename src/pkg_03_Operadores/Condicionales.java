3be99ab10c1d0775f49d1bea4a01a832257dcbf4
Aberu <anrosalesc@gmail.com>
2022-08-23 16:50:07 -0500

    upd

---
+++ src/pkg_03_Operadores/Condicionales.java
@@ -0,0 +1,25 @@
+package pkg_03_Operadores;
+
+public class Condicionales {
+    public static void main(String arg[]) {
+        var a = 10;
+        var valorMinimo = 0;
+        var valorMaximo = 10;
+
+        var resultado = a >= valorMinimo && a <= valorMaximo;
+
+        if (resultado) {
+            System.out.println("Dentro de rango");
+        } else {
+            System.out.println("Fuera de rango");
+        }
+
+        var vacaciones = true;
+        var diaDescanso = false;
+        if (vacaciones || diaDescanso) {
+            System.out.println("Padre puede asistir al juego de su hijo");
+        } else {
+            System.out.println("El padre esta ocupado");
+        }
+    }
+}
---
+++ src/pkg_03_Operadores/Igualdad_Relacionales.java
@@ -0,0 +1,42 @@
+package pkg_03_Operadores;
+
+public class Igualdad_Relacionales {
+    public static void main(String args[]) {
+        var a = 3;
+        var b = 2;
+
+        var c = (a == b);
+        System.out.println("c = " + c);
+
+        var d = a != b;
+        System.out.println("d = " + d);
+
+        var cadena = "Hola";
+        var cadena2 = "Adios";
+        var e = cadena == cadena2; // compara la referencia de objetos
+        System.out.println("e = " + e);
+
+        var f = cadena.equals(cadena2); // comparamos contenido de cadenas
+        System.out.println("f = " + f);
+
+        // ==================== Relacionales=============
+
+        var g = a >= b; // mayor que > o el mayor o igual >=
+        System.out.println("g = " + g);
+
+        if (a % 2 == 0) { // operamos a modulo de 2
+            System.out.println("Es numero par");
+        } else {
+            System.out.println("Es numero impar");
+        }
+
+        var edad = 30;
+        var adulto = 18;
+        if (edad >= adulto) {
+            System.out.println("Es un adulto");
+        } else {
+            System.out.println("Es menor de edad");
+        }
+
+    }
+}
